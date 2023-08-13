7efae6534699693292bf8de15921f0fb429a9a0b
Divyansh Falodiya <divyanshfalodiyaab@gmail.com>
2023-08-13 19:27:13 +0530

    added disconnect func

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,15 @@
 import { config } from "dotenv";
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application } from "express";
 import { createServer } from "http";
-import { Server, Socket } from "socket.io";
+import { Server } from "socket.io";
+import { onCreatePrivateRoomHandler } from "./handlers/socket/rooms";
+import { onSocketConnectHandler } from "./handlers/socket/connection";
 import {
     ClientToServerEvents,
     InterServerEvents,
-    IoType,
     ServerToClientEvents,
     SocketData,
 } from "./types/socket";
-import { onCreatePrivateRoomHandler } from "./handlers/socket/rooms";
-import { onSocketConnectHandler } from "./handlers/socket/connection";
-import { ErrorFromServer } from "./utils/error";
 
 config();
 
@@ -26,14 +24,28 @@ const io = new Server<
     SocketData
 >(httpServer, { cors: { origin: "*" } });
 
+const roomToOwnerMap = new Map<string, string>();
+
+// Socket
 io.on("connection", (socket) => {
     onSocketConnectHandler(io, socket);
 
     socket.on("create-private-room", (callback) => {
         const room = onCreatePrivateRoomHandler(io, socket);
         socket.join(room.roomId);
-        callback(null, new ErrorFromServer("Something went wrong!"));
-        // callback(room);
+        roomToOwnerMap.set(room.roomId, room.ownerId);
+        callback(room);
+    });
+
+    socket.on("disconnect", () => {
+        const joinedRooms = socket.rooms;
+        for (const room in joinedRooms) {
+            // If this socket is the owner of a room, delete that room
+            if (roomToOwnerMap.get(room) === socket.id)
+                roomToOwnerMap.delete(room);
+
+            io.sockets.adapter.rooms.delete(room);
+        }
     });
 });
 
