7543d2d51e2563b3eb0a49be8f8f0bb2e1d442d4
Divyansh Falodiya <divyansh@mercari.com>
2025-01-24 22:37:25 +0900

    feat: add error boundary for handling errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import ErrorBoundary from './components/Error/ErrorBoundary';
+import texts from './constants/texts';
 import GameProvider from './contexts/game/GameContext';
 import SnackbarProvider from './contexts/SnackbarContext';
 import SocketProvider from './contexts/SocketContext';
@@ -27,13 +29,15 @@ function App() {
 
   return (
     <div>
-      <SnackbarProvider>
-        <UserProvider>
-          <SocketProvider>
-            <RouterProvider router={router} />
-          </SocketProvider>
-        </UserProvider>
-      </SnackbarProvider>
+      <ErrorBoundary fallback={<p>{texts.common.error.fallbackText}</p>}>
+        <SnackbarProvider>
+          <UserProvider>
+            <SocketProvider>
+              <RouterProvider router={router} />
+            </SocketProvider>
+          </UserProvider>
+        </SnackbarProvider>
+      </ErrorBoundary>
     </div>
   );
 }
