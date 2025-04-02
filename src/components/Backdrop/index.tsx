d29e1370f21c9cabfe86d330450cd331aba60d5f
Divyansh Falodiya <divyansh@mercari.com>
2025-04-02 15:16:32 +0900

    fix: darker backdrop

--- src/components/Backdrop/index.tsx
+++ src/components/Backdrop/index.tsx
@@ -8,7 +8,7 @@ const Backdrop = ({ children }: PropsWithChildren) => {
     };
   }, []);
   return (
-    <div className="fixed top-0 left-0 w-full h-full text-chalk-yellow bg-black bg-opacity-30">
+    <div className="fixed top-0 left-0 w-full h-full text-chalk-green bg-black bg-opacity-50">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         {children}
       </div>
